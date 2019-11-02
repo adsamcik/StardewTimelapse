05d920991582c90ba25775c0966574a682ef7d1f
adsamcik <adsamcik@users.noreply.github.com>
2019-11-02 07:56:05 +0100

    Improved farm capture names

--- StardewTimelapse/Main.cs
+++ StardewTimelapse/Main.cs
@@ -11,14 +11,13 @@ using System.Threading.Tasks;
 namespace StardewTimelapse {
     /// <summary>The main entry point for the mod.</summary>
     public class ModEntry : Mod {
-        private const string timelapseDirectoryName = "timelapse";
+        private const string timelapseDirectoryBaseName = "timelapse";
         private const string farmMapName = "Farm";
 
         private DirectoryInfo exportDirectory;
         private DirectoryInfo timelapseDirectory;
 
-        private int nextIndex;
-        private bool farmLoaded;
+        private bool isFarmLoaded;
 
         /*********
         ** Public methods
@@ -31,43 +30,34 @@ namespace StardewTimelapse {
         }
 
         private void Initialize() {
+            InitializeExportDirectory();
             SelectTimelapseDirectory();
-            UpdateLastIndex();
         }
 
-        private void SelectTimelapseDirectory() {
+        private string GetDirectoryName() {
+            var playerName = Game1.player.Name;
+            var gameId = Game1.uniqueIDForThisGame;
+            return $"{playerName}-{ gameId}";
+        }
+
+        private string GetScreenshotName() {
+            var date = Game1.Date;
+            var season = date.Season;
+            var day = date.DayOfMonth;
+            return $"{season}-{day}";
+        }
+
+        private void InitializeExportDirectory() {
             var stardewRoot = new DirectoryInfo(Constants.ExecutionPath);
             exportDirectory = stardewRoot.EnumerateDirectories("MapExport").FirstOrDefault();
             if (exportDirectory == null) {
                 exportDirectory = stardewRoot.CreateSubdirectory("MapExport");
             }
-            var playerName = Game1.player.Name;
-            var gameId = Game1.uniqueIDForThisGame;
-            timelapseDirectory = exportDirectory.CreateSubdirectory($"{timelapseDirectoryName}-{playerName}-{gameId}");
-            UpdateLastIndex();
         }
 
-        private void UpdateLastIndex() {
-            var fileEnumeration = timelapseDirectory.EnumerateFiles();
-            int nextIndex = 0;
-
-            if (fileEnumeration.Any()) {
-                var nextIndexCalc = fileEnumeration.Max((file) => {
-                    var nameNoExtension = Path.GetFileNameWithoutExtension(file.Name);
-
-                    if (int.TryParse(nameNoExtension, out var index)) {
-                        return index;
-                    } else {
-                        return int.MinValue;
-                    }
-                });
-
-                if (nextIndexCalc > 0) {
-                    nextIndex = nextIndexCalc;
-                }
-            }
-
-            this.nextIndex = nextIndex;
+        private void SelectTimelapseDirectory() {
+            var directoryName = $"{timelapseDirectoryBaseName}-{GetDirectoryName()}";
+            timelapseDirectory = exportDirectory.CreateSubdirectory(directoryName);
         }
 
         private void OnSaveLoaded(object sender, SaveLoadedEventArgs e) {
@@ -79,7 +69,7 @@ namespace StardewTimelapse {
         private void OnWarped(object sender, WarpedEventArgs e) {
             if (e.NewLocation.Name == farmMapName) {
                 Helper.Events.Player.Warped -= OnWarped;
-                farmLoaded = true;
+                isFarmLoaded = true;
                 CaptureMap();
             }
         }
@@ -89,8 +79,17 @@ namespace StardewTimelapse {
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
         private void OnDayStarted(object sender, DayStartedEventArgs e) {
-            if (farmLoaded) {
+            if (isFarmLoaded) {
                 CaptureMap();
+            }
+        }
+
+        private void MoveMapCapture(FileInfo capture) {
+            var extension = capture.Extension;
+            var desiredPath = Path.Combine(timelapseDirectory.FullName, $"{GetScreenshotName()}{extension}");
+            var desiredFile = new FileInfo(desiredPath);
+            if (!desiredFile.Exists) {
+                capture.MoveTo(desiredFile.FullName);
             }
         }
 
@@ -98,8 +97,7 @@ namespace StardewTimelapse {
             Helper.ConsoleCommands.Trigger("export", new[] { "Farm", "all" });
             var mapExport = exportDirectory.EnumerateFiles().FirstOrDefault((file) => Path.GetFileNameWithoutExtension(file.Name) == farmMapName);
             if (mapExport != null) {
-                var extension = mapExport.Extension;
-                mapExport.MoveTo(Path.Combine(timelapseDirectory.FullName, $"{nextIndex++}{extension}"));
+                MoveMapCapture(mapExport);
             }
         }
     }
